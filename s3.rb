1cfba5d2c9fadf11835979d7c497e5eeb586a207
Kevin Chandler <im.kevin@me.com>
2016-10-01 08:08:55 -0700

    clean up

--- s3.rb
+++ s3.rb
@@ -1,30 +1,37 @@
+# Usage:
+#        S3.upload(contents: 'file_body',
+#                  bucket: 'random',
+#                  file: 'file.txt',
+#                  append_to_file: false)
+
 require 'fog'
 
 module S3
-
   S3_CREDENTIALS = {
-      :bucket => ENV['S3_BUCKET'],
-      :bucket_url => "https://s3.amazonaws.com/#{bucket}",
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  }
+    bucket: ENV['S3_BUCKET'],
+    bucket_url: "https://s3.amazonaws.com/#{bucket}",
+    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+  }.freeze
 
+  def self.upload(contents: contents,
+                  bucket: bucket, file: file,
+                  append_to_file: append_to_file = true)
 
-  # Usage: S3.upload(contents: 'body1', bucket: 'random-misc', file: 'new-file.txt', append_to_file: false)
-  def self.upload(contents: contents, bucket: bucket, file: file, append_to_file: append_to_file=true)
-    Excon.defaults[:ssl_verify_peer] = false if Rails.env.development? # prevents SSL issuse during dev
+    connection = initiate_connection
+    directory = connection.directories.get bucket
 
-    connection = self.initiate_connection
+    if directory.nil?
+      raise "Bucket #{bucket} does not exist. You must create one before"\
+            ' uploading to it.'
+    end
 
     file_opts = {
-      :key => file,
-      :body => contents,
-      :public => false,
-      :bucket_name => bucket
+      key: file,
+      body: contents,
+      public: false,
+      bucket_name: bucket
     }
-
-    directory = connection.directories.get bucket
-    raise "Bucket #{bucket} does not exist. You must create one before uploading to it." if directory.nil?
 
     file = directory.files.get(file) || directory.files.create(file_opts)
     file_contents = append_to_file ? file.body : ''
@@ -34,24 +41,31 @@ module S3
   end
 
   def self.retrieve_file(bucket: bucket, file: file)
-    self.retrieve_bucket(bucket: bucket).files.get file rescue nil
+    retrieve_bucket(bucket: bucket).files.get file
   end
 
   def self.retrieve_bucket(bucket: bucket)
-    self.initiate_connection.directories.get bucket rescue nil
+    initiate_connection.directories.get bucket
   end
 
   def self.initiate_connection
-    if !defined?(S3_CREDENTIALS) || S3_CREDENTIALS[:access_key_id].nil? || S3_CREDENTIALS[:secret_access_key].nil?
-      raise "S3_CREDENTIALS, S3_CREDENTIALS[:access_key_id], and S3_CREDENTIALS[:secret_access_key] must be defined"
-    end
+    check_required
 
-    Fog::Storage.new({
-      :provider => 'AWS',
-      :aws_access_key_id => S3_CREDENTIALS[:access_key_id],
-      :aws_secret_access_key => S3_CREDENTIALS[:secret_access_key],
-      :path_style => true
-    })
+    Fog::Storage.new(
+      provider: 'AWS',
+      aws_access_key_id: S3_CREDENTIALS[:access_key_id],
+      aws_secret_access_key: S3_CREDENTIALS[:secret_access_key],
+      path_style: true
+    )
   end
 
+  def check_required
+    if (!defined?(S3_CREDENTIALS) ||
+        S3_CREDENTIALS[:access_key_id].nil? ||
+        S3_CREDENTIALS[:secret_access_key].nil?)
+
+      raise 'S3_CREDENTIALS, S3_CREDENTIALS[:access_key_id], and '\
+            'S3_CREDENTIALS[:secret_access_key] must be defined'
+    end
+  end
 end
